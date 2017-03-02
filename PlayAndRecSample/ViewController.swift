8c681dbc5dd77573750d112393ed44d616da1018
tad-iizuka <tad.iizuka@gmail.com>
2017-03-02 12:16:53 +0900

    Initial Commit

---
+++ PlayAndRecSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlayAndRecSample
+//
+//  Created by Tadashi on 2017/03/02.
+//  Copyright © 2017 T@d. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
